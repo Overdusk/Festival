039902c8536912f18a241e531572baabab673f59
Overdusk <overduskgaming@gmail.com>
2024-12-16 20:14:01 +0100

    added food and maps in landing

--- src/components/ExpFloor.jsx
+++ src/components/ExpFloor.jsx
@@ -1,4 +1,5 @@
 import '../css/ExpFloor.css';
+import { Link } from 'react-router-dom'
 function ExpFloor()
 {
 	return(
@@ -12,10 +13,23 @@ function ExpFloor()
 					<div className="camping-column">
 						<span>CAMPING</span>
 						<p>Es un momento increíble para conectar con la naturaleza, con la música y pasar un buen momento con tus amigos. Llénate de nuevas experiencias dentro del festival.</p>
-						<a href="#"> Mas Información </a>
+						<Link to="#"> Mas Información </Link>
 					</div>
 					<img src="/3.png" className="camping-image" />
-					
+				</div>
+			</div>
+			<div className="food-and-map">
+				<div className="food-section">
+					<img src="/1.jpg" className="foodimg" />
+					<span className="food-title">FOODIE LIFE</span>
+					<span>Eso que se te antoja para el monchis lo encuentras acá</span>
+					<Link to="/artists">Mas Información</Link>
+				</div>
+				<div className="food-section">
+					<img src="/1.jpg" className="foodimg" />
+					<span className="food-title">FOODIE LIFE</span>
+					<span>Eso que se te antoja para el monchis lo encuentras acá</span>
+					<Link to="/artists">Mas Información</Link>
 				</div>
 			</div>
 		</>
